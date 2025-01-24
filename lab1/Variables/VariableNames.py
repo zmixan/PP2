3ee92d3588dc1077933f8305eca5e978e0a003a5
zmixan <zmixann@gmail.com>
2025-01-24 19:09:35 +0500

    Create VariableNames.py

---
+++ lab1/Variables/VariableNames.py
@@ -0,0 +1,15 @@
+#legal
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+#illegal
+2myvar = "John"
+my-var = "John"
+my var = "John"
+#Multi Words
+myVariableName = "John" 
+MyVariableName = "John"
+my_variable_name = "John"
