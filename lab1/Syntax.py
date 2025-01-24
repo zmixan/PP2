ebe78f2af5b9bef439239900ea7b40e00673b53d
zmixan <zmixann@gmail.com>
2025-01-24 19:02:15 +0500

    Update Syntax.py

--- lab1/Syntax.py
+++ lab1/Syntax.py
@@ -1,2 +1,17 @@
+#correct syntax
 if 5 > 2:
   print("Five is greater than two!")
+
+if 5 > 2:
+ print("Five is greater than two!") 
+
+if 5 > 2:
+        print("Five is greater than two!") 
+  
+#Syntax Error
+if 5 > 2:
+print("Five is greater than two!")
+
+if 5 > 2:
+ print("Five is greater than two!")
+        print("Five is greater than two!") 
