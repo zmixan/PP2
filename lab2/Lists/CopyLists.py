8c7cc5fde15aa636c39cce7b6620ddb0f193298b
zmixan <zmixann@gmail.com>
2025-01-25 13:34:23 +0500

    Create CopyLists.py

---
+++ lab2/Lists/CopyLists.py
@@ -0,0 +1,11 @@
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist[:]
+print(mylist)
