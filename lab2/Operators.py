91d2fb07499da8fbb53c14702568ed0773da6fe1
zmixan <zmixann@gmail.com>
2025-01-25 08:55:57 +0500

    Create Operators.py

---
+++ lab2/Operators.py
@@ -0,0 +1,61 @@
+print(10 + 5) 
+
+#operators
++ 	Addition 	
+- 	Subtraction 
+* 	Multiplication 	
+/ 	Division 	
+% 	Modulus 	
+** 	Exponentiation 	
+// 	Floor division 	
+
+#Assighment 
+x = 5 	        x = 5 	
+x += 3          x = x + 3 	
+x -= 3       	  x = x - 3 	
+x *= 3 	        x = x * 3 	
+x /= 3 	        x = x / 3 	
+x %= 3 	        x = x % 3 	
+x //= 3 	      x = x // 3 	
+x **= 3 	      x = x ** 3 	
+x &= 3 	        x = x & 3 	
+x |= 3 	        x = x | 3 	
+x ^= 3 	        x = x ^ 3 	
+x >>= 3       	x = x >> 3 	
+x <<= 3       	x = x << 3 	
+print(x := 3) 	x = 3
+                print(x)
+
+#Comparison 
+== 	Equal 
+!= 	Not equal 	
+> 	Greater than 	
+< 	Less than 	
+>= 	Greater than or equal to 
+<= 	Less than or equal to
+
+#logical 
+and  	Returns True if both statements are true 	 	
+or   	Returns True if one of the statements is true 	
+not 	Reverse the result, returns False if the result is true
+
+#identity
+is  	Returns True if both variables are the same object 	
+is not 	Returns True if both variables are not the same object
+
+#Membership
+in  	Returns True if a sequence with the specified value is present in the object 	
+not in 	Returns True if a sequence with the specified value is not present in the object
+
+#Bitwise
+&  	AND 	Sets each bit to 1 if both bits are 1 
+| 	OR 	Sets each bit to 1 if one of two bits is 1 	
+^ 	XOR 	Sets each bit to 1 if only one of two bits is 1 	
+~ 	NOT 	Inverts all the bits 	
+<< 	Zero fill left shift 	Shift left by pushing zeros in from the right and let the leftmost bits fall off 	
+>> 	Signed right shift 	Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
+
+#Precedence
+print((6 + 3) - (6 + 3)) 
+print(100 + 5 * 3) 
+  
