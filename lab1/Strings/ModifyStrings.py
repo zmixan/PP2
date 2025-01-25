c4082a1593c2ebb04d258793022d709780b47750
zmixan <zmixann@gmail.com>
2025-01-25 08:06:28 +0500

    Create ModifyStrings.py

---
+++ lab1/Strings/ModifyStrings.py
@@ -0,0 +1,12 @@
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.lower())
+
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!'] 
+
