a17b39163e2b6d6b9211dba32cd0d1c9df4dd5da
zmixan <zmixann@gmail.com>
2025-01-25 08:08:50 +0500

    Create StringFormat.py

---
+++ lab1/Strings/StringFormat.py
@@ -0,0 +1,19 @@
+age = 36
+txt = "My name is John, I am " + age
+print(txt)
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
+
