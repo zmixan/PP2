d3287fc23564fee4cf1fc2ec91286eab8ac5857c
zmixan <zmixann@gmail.com>
2025-01-25 07:59:03 +0500

    Create Strings.py

---
+++ lab1/Strings/Strings.py
@@ -0,0 +1,22 @@
+print("Hello")
+#same as
+print('Hello')
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+a = "Hello"
+print(a) 
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a) 
+
+ a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
